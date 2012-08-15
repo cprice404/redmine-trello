b59fae098823c121856ec286c24b7b7277f6939c
Andy Parker <andy@puppetlabs.com>
2012-08-15 15:15:03 -0700

    Update board lister to look at all boards

--- bin/show_lists_for_board.rb
+++ bin/show_lists_for_board.rb
@@ -3,6 +3,7 @@
 $LOAD_PATH << File.join(File.absolute_path(File.dirname(__FILE__)), "..", "config")
 $LOAD_PATH << File.join(File.absolute_path(File.dirname(__FILE__)), "..", "lib")
 
+require 'rmt_config'
 require 'redmine_trello_conf'
 require 'trello_utils'
 
@@ -17,20 +18,21 @@ unless board_id = ARGV[0]
   exit(1)
 end
 
-TrelloUtils.initialize_auth(RMTConfig::Trello::AppKey,
-                            RMTConfig::Trello::Secret,
-                            RMTConfig::Trello::UserToken)
+RMTConfig.mappings.each do |mapping|
+	TrelloUtils.initialize_auth(mapping.trello.app_key,
+															mapping.trello.secret,
+															mapping.trello.user_token)
 
-begin
-  board = Trello::Board.find(board_id)
-rescue Trello::Error => e
-  puts "Unable to find board with id '#{board_id}'. (#{e.to_s.strip()})"
-  exit(1)
+	begin
+		board = Trello::Board.find(board_id)
+	rescue Trello::Error => e
+		puts "Unable to find board with id '#{board_id}'. (#{e.to_s.strip()})"
+		exit(1)
+	end
+
+	puts "Board found; name: '#{board.name}'"
+
+	board.lists.each do |list|
+		puts "\tList '#{list.name}' has id '#{list.id}'"
+	end
 end
-
-puts "Board found; name: '#{board.name}'"
-
-board.lists.each do |list|
-  puts "\tList '#{list.name}' has id '#{list.id}'"
-end
-
