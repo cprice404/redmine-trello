309785eac5230338547b02f2b18c08a17e12b4c5
Andrew Parker <andy@puppetlabs.com>
2012-08-16 14:54:21 -0700

    Encapsulate synchronization logic in a class

--- bin/copy_tickets_from_redmine_to_trello.rb
+++ bin/copy_tickets_from_redmine_to_trello.rb
@@ -19,34 +19,50 @@ require 'rmt/redmine'
 require 'rmt/trello'
 require 'rmt/synchronization_data'
 
-mappings = RMT::Config.mappings
 
-trello_list_data = mappings.inject({}) do |issue_buckets, mapping|
-  issues = (issue_buckets[mapping.trello] ||= [])
-  redmine_client = RMT::Redmine.new(mapping.redmine.base_url,
-                                    mapping.redmine.username,
-                                    mapping.redmine.password)
+class Synchronize
+  def initialize
+    @trello_list_data = {}
+  end
 
-  redmine_issues = redmine_client.get_issues_for_project(mapping.redmine.project_id, :status => RMT::Redmine::Status::Unreviewed)
-  issues.concat(redmine_issues.collect &RMT::SynchronizationData.from_redmine(mapping.trello))
-  issue_buckets
-end
+  def synchronize(data, to_trello)
+    list_data = (@trello_list_data[to_trello] ||= [])
+    list_data.concat(data)
+    self
+  end
 
-trello_list_data.each do |list, data|
-  trello = RMT::Trello.new(list.app_key,
-                           list.secret,
-                           list.user_token)
+  def finish
+    @trello_list_data.each do |list, data|
+      trello = RMT::Trello.new(list.app_key,
+                               list.secret,
+                               list.user_token)
 
-  existing_cards = trello.list_cards_in(list.target_list_id)
+      existing_cards = trello.list_cards_in(list.target_list_id)
 
-  existing_cards.
-    reject { |card| data.any? { |data| data.is_data_for? card } }.
-    each do |card|
-      puts "Removing card: #{card.name}"
-      trello.archive_card(card)
+      existing_cards.
+        reject { |card| data.any? { |data| data.is_data_for? card } }.
+        each do |card|
+          puts "Removing card: #{card.name}"
+          trello.archive_card(card)
+        end
+
+      data.
+        reject { |data| existing_cards.any? { |card| data.is_data_for? card } }.
+        each { |data| data.insert_into(trello) }
     end
-
-  data.
-    reject { |data| existing_cards.any? { |card| data.is_data_for? card } }.
-    each { |data| data.insert_into(trello) }
+  end
 end
+
+RMT::Config.
+  mappings.
+  inject(Synchronize.new) do |sync, mapping|
+    redmine_client = RMT::Redmine.new(mapping.redmine.base_url,
+                                      mapping.redmine.username,
+                                      mapping.redmine.password)
+
+    redmine_issues = redmine_client.get_issues_for_project(mapping.redmine.project_id, :status => RMT::Redmine::Status::Unreviewed)
+
+    sync.synchronize(redmine_issues.collect(&RMT::SynchronizationData.from_redmine(mapping.trello)),
+                     mapping.trello)
+  end.
+  finish
